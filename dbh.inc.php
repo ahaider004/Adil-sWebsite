ee46421eaa664899966d0fe7ac7a6dda9f366bf2
ahaider004 <59404010+ahaider004@users.noreply.github.com>
2020-01-09 00:07:56 -0500

    Add files via upload

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost','root', '', 'commentsection');
+$conn = mysqli_connect('localhost','root', '', 'galleryexample');
 if (!$conn) {
 	die("Connection Failed: ".mysqli_connect_error());
 }
